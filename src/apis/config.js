46f003a7f0bf904066e4a1704d449c66471f3eba
Hammad Farooq <hammad@cheetahagency.com>
2024-05-17 12:25:17 +0500

    refactor: Update API base url

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "http://170.64.198.245:8080";
-export const BASE_URL = "https://bot.koreannoodles.pk";
+export const BASE_URL = "https://api.customerbot.ai";
 // export const BASE_URL = "https://ai-backend-five.vercel.app";
