9b16b3946b4ecd2e684428d516ae330072c0ea69
Hammad Farooq <hammad@cheetahagency.com>
2024-07-31 21:22:05 +0500

    Update config.js

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "http://170.64.198.245:8080";
-// export const BASE_URL = "https://api.customerbot.ai";
-export const BASE_URL = "https://staging.customerbot.ai";
+export const BASE_URL = "https://api.customerbot.ai";
+// export const BASE_URL = "https://staging.customerbot.ai";
 // export const BASE_URL = "https://ai-backend-five.vercel.app";
